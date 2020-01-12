79f602c065dbe5fe38328bf28e3fb3e734725d77
Joshua Uzo <nwankwo.joshua@gmail.com>
2020-01-12 23:08:44 +1100

    Formatted groovy files

--- src/main/resources/templates/groovyTemplates/views/home.groovy
+++ src/main/resources/templates/groovyTemplates/views/home.groovy
@@ -1,5 +1,4 @@
 layout 'layouts/main.tpl',
-        pageTitle: 'Spring Boot - Groovy templates example with layout',
-        mainBody: contents {
-            div("This is an application using Boot $bootVersion and Groovy templates $groovyVersion")
-        }
+pageTitle: 'Spring Boot - Groovy templates example with layout',
+mainBody: contents { div("This is an application using Boot $bootVersion and Groovy templates $groovyVersion") }
+
